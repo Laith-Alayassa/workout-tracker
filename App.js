f2692faddf7b522bedbf8f166b9beac03a797ef4
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-12-24 23:04:48 -0800

    added home screen and theme

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
+import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaView } from "react-native";
 import HomeScreen from "./screens/HomeScreen";
 import GlobalStyles from "./styles/GlobalStyles";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import { theme } from "./styles/theme";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <SafeAreaView style={GlobalStyles.droidSafeArea}>
-      <HomeScreen></HomeScreen>
-    </SafeAreaView>
+    <PaperProvider theme={theme}>
+      <SafeAreaView style={GlobalStyles.droidSafeArea}>
+        <NavigationContainer theme={theme}>
+          <Stack.Navigator>
+            <Stack.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                headerTitleAlign: "center",
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
+    </PaperProvider>
   );
 }
