8f85ad9b8d2d4fe9474d1d386185c3ddab6123cb
ShawermaLVR <60319236+Laith-Alayassa@users.noreply.github.com>
2022-12-27 12:52:41 -0800

    styling create temp form

--- screens/CreateTemplate.js
+++ screens/CreateTemplate.js
@@ -1,11 +1,25 @@
 import React, { useState } from "react";
-import { Button, TextInput, View, StyleSheet } from "react-native";
+import {
+  Button,
+  TextInput,
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+} from "react-native";
 import { Formik } from "formik";
 import { writeTemplateData } from "../data/firestopreRealTime";
 
 const CreateTemplate = () => {
   const [workoutsNum, setWorkoutsNum] = useState(Array(3).fill(0));
-
+  const addField = () => {
+    setWorkoutsNum((workoutsNum) => {
+      let newWorkouts = workoutsNum.slice();
+      newWorkouts.push(1);
+      console.log(newWorkouts);
+      return newWorkouts;
+    });
+  };
   return (
     <Formik
       enableReinitialize={true}
@@ -13,48 +27,43 @@ const CreateTemplate = () => {
       onSubmit={(values) => console.log(values)}
     >
       {({ handleChange, handleBlur, handleSubmit, values }) => (
-        <View>
-          <TextInput
-            style={styles.textInput}
-            onChangeText={handleChange("templateName")}
-            onBlur={handleBlur("templateName")}
-            value={values.templateName}
-          />
-          {workoutsNum.map((exercise, i) => {
-            return (
-              <TextInput
-                style={styles.textInput}
-                onChangeText={handleChange(`exercise${i}`)}
-                onBlur={handleBlur(`exercise${i}`)}
-              />
-            );
-          })}
-          {/* <TextInput
-            style={styles.textInput}
-            onChangeText={handleChange("exercise1")}
-            onBlur={handleBlur("exercise1")}
-            value={values.exercise1}
-          />
+        <ScrollView>
+          <View>
+            <View style={styles.workoutTitleContainer}>
+              <Text style={styles.workoutTitle}>⚙️ Workout Title ⚙️</Text>
+            </View>
+            <TextInput
+              style={[
+                styles.textInput,
+                { marginHorizontal: 20, marginBottom: 20 },
+              ]}
+              placeholder="Push Workout 1"
+              onChangeText={handleChange("templateName")}
+              onBlur={handleBlur("templateName")}
+              value={values.templateName}
+            />
 
-          <TextInput
-            style={styles.textInput}
-            onChangeText={handleChange("exercise2")}
-            onBlur={handleBlur("exercise2")}
-            value={values.exercise2}
-          /> */}
-          <Button
-            onPress={() =>
-              setWorkoutsNum((workoutsNum) => {
-                let newWorkouts = workoutsNum.slice();
-                newWorkouts.push(1);
-                console.log(newWorkouts);
-                return newWorkouts;
-              })
-            }
-            title="add"
-          />
-          <Button onPress={handleSubmit} title="Submit" />
-        </View>
+            {workoutsNum.map((exercise, i) => {
+              return (
+                <>
+                  <View style={styles.set}>
+                    <View style={styles.titleContainer}>
+                      <Text style={styles.title}>{`Exercise ${i + 1}`}</Text>
+                    </View>
+                    <TextInput
+                      placeholder="Exercise Name"
+                      style={styles.textInput}
+                      onChangeText={handleChange(`exercise${i}`)}
+                      onBlur={handleBlur(`exercise${i}`)}
+                    />
+                  </View>
+                </>
+              );
+            })}
+            <Button onPress={addField} title="add" />
+            <Button onPress={handleSubmit} title="Submit" />
+          </View>
+        </ScrollView>
       )}
     </Formik>
   );
@@ -62,9 +71,82 @@ const CreateTemplate = () => {
 
 const styles = StyleSheet.create({
   textInput: {
-    backgroundColor: "grey",
-    borderColor: "blue",
+    backgroundColor: "rgba(158, 150, 150, .2)",
+    borderColor: "rgba(158, 150, 150, .5)",
+    borderRadius: 8,
+    padding: 5,
+    fontSize: 25,
     borderWidth: 1,
+  },
+  workoutTitle: {
+    fontWeight: "bold",
+    fontSize: 24,
+  },
+  workoutTitleContainer: {
+    margin: 50,
+    marginBottom: 20,
+    alignItems: "center",
+  },
+  set: {
+    marginBottom: 25,
+    marginHorizontal: 25,
+    borderColor: "rgba(158, 150, 150, .5)",
+    borderRadius: 15,
+    backgroundColor: "white",
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+  titleContainer: {
+    backgroundColor: "#007AFF",
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 10,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+  title: {
+    fontWeight: "500",
+    fontSize: 18,
+    color: "white",
+  },
+  checkbox: {
+    margin: 10,
+
+    backgroundColor: "rgba(158, 150, 150, .2)",
+    borderRadius: 8,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+
+    elevation: 6,
+  },
+  checkboxAndroid: {
+    margin: 10,
+  },
+  setRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
   },
 });
 
